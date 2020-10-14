a64f1579816892e1295a58bd9f5a7bc573b1fa87
brownliuinnz <brownliuinnz@gmail.com>
2020-10-14 22:06:01 +1300

    Add project files.

---
+++ MVC_Test/Model/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Test.Model
+{
+    public class Book
+    {   [Required]
+        public String name { get; set; }
+        [Key]
+        public int Id { get; set; }
+
+        public int star { get; set; }
+        [Required]
+        public String Author { get; set; }
+    }
+
+}
